46c4f47c05fbf9b0ee4d5e75c79c3cad63fdabcf
ArefOrumiehei <areforumiehei@gmail.com>
2025-06-05 12:24:26 +0330

    Add more colors

--- py_v/helpers/colorize.py
+++ py_v/helpers/colorize.py
@@ -5,6 +5,15 @@ def colorize(text, color):
         'yellow': '\033[93m',
         'orange': '\033[33m',
         'white': '\033[97m',
+        'magenta': '\033[95m',
+        'cyan': '\033[96m',
+        'gray': '\033[90m',
+        'orange': '\033[33m',
+        'lightblue': '\033[94m',
+        'lightgreen': '\033[92m',
+        'lightcyan': '\033[96m',
+        'lightred': '\033[91m',
+        'lightmagenta': '\033[95m',
         'reset': '\033[0m'
     }
     return f"{colors.get(color, colors['white'])}{text}{colors['reset']}"
